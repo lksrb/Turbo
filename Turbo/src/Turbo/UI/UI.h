9aedc9451177852c46494eb0756fb9c61c6b8a87
lksrb <ezyn69@gmail.com>
2023-03-19 22:21:20 +0100

    Update UI.h

--- Turbo/src/Turbo/UI/UI.h
+++ Turbo/src/Turbo/UI/UI.h
@@ -15,7 +15,7 @@ namespace Turbo::UI
     // Display icon buttons
     bool ImageButton(Ref<Texture2D> texture, const ImVec2& size, const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1), i32 frame_padding = -1, const ImVec4& bg_col = ImVec4(0, 0, 0, 0), const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
 
-    // Extends standard DragScalars
+    // Extends imgui standard DragScalars
     bool DragByte(const char* label, char* v, float v_speed = 1.0f, char v_min = 0, char v_max = 0, const char* format = NULL, ImGuiSliderFlags flags = 0);
     bool DragUByte(const char* label, unsigned char* v, float v_speed = 1.0f, unsigned char v_min = 0, unsigned char v_max = UCHAR_MAX, const char* format = NULL, ImGuiSliderFlags flags = 0);
     bool DragInt(const char* label, int* v, float v_speed = 1.0f, int v_min = 0, int v_max = 0, const char* format = NULL, ImGuiSliderFlags flags = 0);
